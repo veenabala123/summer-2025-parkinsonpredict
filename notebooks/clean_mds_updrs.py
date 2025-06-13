d8f81af3b517aef3b6c5aae25e3959c2cac92a89
veenabala123 <93620785+veenabala123@users.noreply.github.com>
2025-06-13 15:05:36 -0500

    Update clean_mds_updrs.py

--- notebooks/clean_mds_updrs.py
+++ notebooks/clean_mds_updrs.py
@@ -1,15 +1,15 @@
 import numpy as np
 import pandas as pd
 
-df = pd.read_csv("MDS-UPDRS_Part_III_PATNO_NHY_ORIGENTRY_02Jun2025.csv",
+df = pd.read_csv("MDS-UPDRS_Part_III_02Jun2025.csv",
                  na_values=["", " ", "N/A"])
+
+columns_kept = ["PATNO","NHY","EVENT_ID"]
+df = df.drop(columns=df.columns.difference(columns_kept))
 
 df.replace(r"^\s*$", np.nan, regex=True, inplace=True)
 df = df.dropna()
-df = df[~(df == 101).any(axis=1)]
-
-columns_kept = ["PATNO","NHY","Origin_of_entry","EVENT_ID"]
-result = df.drop(columns=df.columns.difference(columns_kept))
+result = df[~(df == 101).any(axis=1)]
 
 result.to_csv("clean_mds_updrs.csv", index=False)
 print(result)
